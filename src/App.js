cd54e262a986daf5afe338580980d44b94d6fca8
jef-loppes-reis <jefersondip25@gmail.com>
2024-04-30 21:07:10 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Mensagem from './components/Mensagem';
+import Entrada from './components/Entrada';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Mensagem></Mensagem>
+        <Entrada></Entrada>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Entrada.js
@@ -0,0 +1,11 @@
+function Entrada(){
+    return (
+        <div>
+            <h5>
+                Aqui vai ser digitado algo.
+            </h5>
+        </div>
+    )
+}
+
+export default Entrada;
